73db7758bd39beb78386602fe009399442813936
BerdovOleg <ssedovo@gmail.com>
2022-08-22 22:48:09 +0500

    Add new Sound

--- Assets/Scripts/HelixJump/GamePlay/SoundManger.cs
+++ Assets/Scripts/HelixJump/GamePlay/SoundManger.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class SoundManger : MonoBehaviour
 {
-    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource audioSourceMain;
+    [SerializeField] AudioSource AudioSourceBG;
+
+    [SerializeField] AudioClip BackgroundMusic;
+
+    [Range(0, 1)] public float Volume;
     public void PlaySound(AudioClip clip)
     {
-        audioSource.clip = clip;
-        audioSource.Play();
+        audioSourceMain.clip = clip;
+        audioSourceMain.Play();
+    }
+
+    private void Start()
+    {
+        Volume = .15f;
+        AudioSourceBG.clip=BackgroundMusic;
+        AudioSourceBG.Play();
+        AudioSourceBG.volume = Volume;
+        AudioSourceBG.loop = true;
     }
 }
